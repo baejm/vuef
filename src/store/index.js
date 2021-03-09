5883aa15a25da1d61d8daed0fca276ee0651d864
baejm <jukman81@gmail.com>
2021-03-09 23:45:20 +0900

    로그인하기

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,15 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    editable: true
+    editable: true,
+    fireUser: ""
   },
   mutations: {
     setEdit(state, edit) {
       state.editable = edit;
+    },
+    setFireUser(state, fu) {
+      state.fireUser = fu;
     }
   },
   actions: {},
