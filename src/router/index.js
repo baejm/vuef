06b69e3f61cb0228fccd601772cafbb135414daf
jukman <jukman@naver.com>
2021-03-11 11:36:47 +0900

    storage 주소값받기

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,11 @@ const routes = [
     path: "/board",
     name: "board",
     component: () => import("../views/board/")
+  },
+  {
+    path: "/storage",
+    name: "storage",
+    component: () => import("../views/storage")
   }
 ];
 
---
+++ src/views/storage.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-sheet>
+    sss
+    <v-btn @click="write">write</v-btn>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    async write() {
+      const sn = await this.$firebase
+        .storage()
+        .ref()
+        .child("xxx.text")
+        .putString("abc");
+      const url = await sn.ref.getDownloadURL();
+      console.log(url);
+    }
+  }
+};
+</script>
+
+<style></style>
