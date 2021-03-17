1093a36ab74b1e627355daff1420465817cac3d2
jukman <jukman@naver.com>
2021-03-17 17:25:30 +0900

    73 리스트 모드 추가하기

--- src/util/getSummary.js
+++ src/util/getSummary.js
@@ -1,19 +1,19 @@
-export default (text, limit) => {
-  if (!text.length) return "";
-  let summary = "내용 없음";
-  const ts = text.split("\n");
-  const ss = [];
+export default (text, limit, filter) => {
+  if (!text.length) return ''
+  let summary = '내용 없음'
+  const ts = text.split('\n')
+  const ss = []
   for (const t of ts) {
-    const index = t.indexOf("data:image");
-    if (index >= 0) continue;
-    if (t.length) ss.push(t);
+    const index = t.indexOf(filter)
+    if (index >= 0) continue
+    if (t.length) ss.push(t)
   }
-  const s = ss.join("\n");
+  const s = ss.join('\n')
   if (s.length > limit) {
-    summary = s.substr(0, limit);
-    summary += "\n...";
+    summary = s.substr(0, limit)
+    summary += '\n...'
   } else {
-    summary = s;
+    summary = s
   }
-  return summary;
-};
+  return summary
+}
