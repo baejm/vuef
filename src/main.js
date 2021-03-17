e5667cfce21be7e0c62b8e303b1ed2dba6bc7ec8
jukman <jukman@naver.com>
2021-03-17 14:57:23 +0900

    66 게시물 쓰레드 형식으로 변경하기

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
-import "./plugins/firebase";
-import "./plugins/editor";
-import "./plugins/moment";
-import "./error";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import './plugins/firebase'
+import './plugins/editor'
+import './plugins/moment'
+import './error'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   vuetify,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
