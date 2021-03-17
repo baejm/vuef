2b84cef974c95554a3849b81af364b6937d4af20
jukman <jukman@naver.com>
2021-03-17 11:36:05 +0900

    62 게시물 마무리

--- src/components/board/article/content.vue
+++ src/components/board/article/content.vue
@@ -1,15 +1,19 @@
 <template>
-  <v-container fluid>
-    <v-card v-if="article">
-      <v-toolbar color="info" dark dense flat>
+  <v-container fluid :class="$vuetify.breakpoint.xs ? 'pa-0' : ''">
+    <v-card v-if="article" outlined>
+      <v-toolbar color="transparent" dense flat>
         <v-toolbar-title>
-          {{ article.title }}
+          <v-chip color="info" label class="mr-4">뉴스</v-chip>
+          {{article.title}}
         </v-toolbar-title>
-        <v-spacer />
-        <v-btn @click="articleWrite" icon><v-icon>mdi-pencil</v-icon></v-btn>
-        <v-btn @click="remove" icon><v-icon>mdi-delete</v-icon></v-btn>
+        <v-spacer/>
+        <template v-if="(fireUser && fireUser.uid === article.uid) || (user && user.level === 0)">
+          <v-btn @click="articleWrite" icon><v-icon>mdi-pencil</v-icon></v-btn>
+          <v-btn @click="remove" icon><v-icon>mdi-delete</v-icon></v-btn>
+        </template>
         <v-btn @click="back" icon><v-icon>mdi-close</v-icon></v-btn>
       </v-toolbar>
+      <v-divider/>
       <v-card-text>
         <viewer v-if="content" :initialValue="content"></viewer>
         <v-container v-else>
@@ -19,47 +23,56 @@
         </v-container>
       </v-card-text>
       <v-card-actions>
-        <v-spacer />
+        <v-spacer/>
         <span class="font-italic caption">
           작성일: <display-time :time="article.createdAt"></display-time>
         </span>
       </v-card-actions>
       <v-card-actions>
-        <v-spacer />
+        <v-spacer/>
         <span class="font-italic caption">
           수정일: <display-time :time="article.updatedAt"></display-time>
         </span>
       </v-card-actions>
       <v-card-actions>
-        <v-spacer />
-        <v-btn icon @click="like">
-          <v-icon :color="liked ? 'success' : ''">mdi-thumb-up</v-icon>
-          <span>{{ article.likeCount }}</span>
+        <v-spacer/>
+        <span class="font-italic caption mr-4">
+          작성자:
+        </span>
+        <display-user :user="article.user"></display-user>
+      </v-card-actions>
+      <v-card-actions>
+        <v-spacer/>
+        <v-sheet class="mr-4">
+          <v-icon left>mdi-eye</v-icon>
+          <span class="body-2">{{article.readCount}}</span>
+        </v-sheet>
+        <v-sheet class="mr-0">
+          <v-icon left>mdi-comment</v-icon>
+          <span class="body-2">{{article.commentCount}}</span>
+        </v-sheet>
+        <v-btn text @click="like" :color="liked ? 'success' : ''">
+          <v-icon left>mdi-thumb-up</v-icon>
+          <span>{{article.likeCount}}</span>
         </v-btn>
       </v-card-actions>
-      <v-divider />
+      <v-divider/>
       <v-card-actions class="py-0">
         <v-row no-gutters>
           <v-col cols="4">
-            <v-btn text block color="primary" @click="go(-1)"
-              ><v-icon left>mdi-menu-left</v-icon> 이전</v-btn
-            >
+            <v-btn block text color="primary" @click="go(-1)"><v-icon left>mdi-menu-left</v-icon> 이전</v-btn>
           </v-col>
           <v-col cols="4" class="d-flex">
-            <v-divider vertical />
-            <v-btn text block color="primary" @click="back"
-              ><v-icon left>mdi-format-list-bulleted</v-icon> 목록</v-btn
-            >
-            <v-divider vertical />
+            <v-divider vertical></v-divider>
+            <v-btn block text color="primary" @click="back"><v-icon left>mdi-format-list-bulleted-square</v-icon> 목록</v-btn>
+            <v-divider vertical></v-divider>
           </v-col>
           <v-col cols="4">
-            <v-btn text block color="primary" @click="go(1)"
-              ><v-icon left>mdi-menu-right</v-icon> 다음</v-btn
-            >
+            <v-btn block text color="primary" @click="go(1)"><v-icon left>mdi-menu-right</v-icon> 다음</v-btn>
           </v-col>
         </v-row>
       </v-card-actions>
-      <v-divider />
+      <v-divider/>
       <display-comment :article="article" :docRef="ref"></display-comment>
     </v-card>
     <v-card v-else>
@@ -70,131 +83,117 @@
       </v-container>
     </v-card>
   </v-container>
+
 </template>
 <script>
-import axios from "axios";
-import DisplayTime from "@/components/display-time";
-import DisplayComment from "@/components/display-comment";
+import axios from 'axios'
+import DisplayTime from '@/components/display-time'
+import DisplayComment from '@/components/display-comment'
+import DisplayUser from '@/components/display-user'
 
 export default {
-  components: { DisplayTime, DisplayComment },
-  props: ["boardId", "articleId"],
-  data() {
+  components: { DisplayTime, DisplayComment, DisplayUser },
+  props: ['boardId', 'articleId'],
+  data () {
     return {
-      content: "",
+      content: '',
       ref: null,
       unsubscribe: null,
       article: null,
       doc: null
-    };
+    }
   },
   computed: {
-    fireUser() {
-      return this.$store.state.fireUser;
+    user () {
+      return this.$store.state.user
     },
-    liked() {
-      if (!this.fireUser) return false;
-      return this.article.likeUids.includes(this.fireUser.uid);
+    fireUser () {
+      return this.$store.state.fireUser
+    },
+    liked () {
+      if (!this.fireUser) return false
+      return this.article.likeUids.includes(this.fireUser.uid)
     }
   },
   watch: {
-    articleId() {
-      this.subscribe();
+    articleId () {
+      this.subscribe()
     }
   },
-  created() {
-    this.subscribe();
+  async created () {
+    this.subscribe()
   },
-  destroyed() {
-    if (this.unsubscribe) this.unsubscribe();
+  destroyed () {
+    if (this.unsubscribe) this.unsubscribe()
   },
   methods: {
-    subscribe() {
-      if (this.unsubscribe) this.unsubscribe();
-      this.ref = this.$firebase
-        .firestore()
-        .collection("boards")
-        .doc(this.boardId)
-        .collection("articles")
-        .doc(this.articleId);
+    subscribe () {
+      window.scrollTo(0, 0)
+      if (this.unsubscribe) this.unsubscribe()
+      this.ref = this.$firebase.firestore().collection('boards').doc(this.boardId).collection('articles').doc(this.articleId)
       this.ref.update({
         readCount: this.$firebase.firestore.FieldValue.increment(1)
-      });
+      })
       this.unsubscribe = this.ref.onSnapshot(doc => {
         if (!doc.exists) {
-          this.back();
-          return;
+          this.back()
+          return
         }
-        this.doc = doc;
-        const item = doc.data();
-        item.createdAt = item.createdAt.toDate();
-        item.updatedAt = item.updatedAt.toDate();
-        if (!this.article || this.article.url !== item.url)
-          this.fetch(item.url);
-        this.article = item;
-      }, console.error);
+        this.doc = doc
+        const item = doc.data()
+        item.createdAt = item.createdAt.toDate()
+        item.updatedAt = item.updatedAt.toDate()
+        if (!this.article || this.article.url !== item.url) this.fetch(item.url)
+        this.article = item
+      }, console.error)
     },
-    async fetch(url) {
-      this.content = "";
-      const r = await axios.get(url);
-      this.content = typeof r.data === "string" ? r.data : r.data.toString();
+    async fetch (url) {
+      this.content = ''
+      const r = await axios.get(url)
+      this.content = typeof r.data === 'string' ? r.data : r.data.toString()
     },
-    async articleWrite() {
-      this.$router.push({ path: this.$route.path, query: { action: "write" } });
+    async articleWrite () {
+      this.$router.push({ path: this.$route.path, query: { action: 'write' } })
     },
-    async remove() {
-      await this.ref.delete();
+    async remove () {
+      await this.ref.delete()
     },
-    back() {
-      const us = this.$route.path.split("/");
-      us.pop();
-      this.$router.push({ path: us.join("/") });
+    back () {
+      const us = this.$route.path.split('/')
+      us.pop()
+      this.$router.push({ path: us.join('/') })
     },
-    async like() {
-      if (!this.fireUser) throw Error("로그인이 필요합니다");
+    async like () {
+      if (!this.fireUser) throw Error('로그인이 필요합니다')
       if (this.liked) {
         await this.ref.update({
           likeCount: this.$firebase.firestore.FieldValue.increment(-1),
-          likeUids: this.$firebase.firestore.FieldValue.arrayRemove(
-            this.fireUser.uid
-          )
-        });
+          likeUids: this.$firebase.firestore.FieldValue.arrayRemove(this.fireUser.uid)
+        })
       } else {
         await this.ref.update({
           likeCount: this.$firebase.firestore.FieldValue.increment(1),
-          likeUids: this.$firebase.firestore.FieldValue.arrayUnion(
-            this.fireUser.uid
-          )
-        });
+          likeUids: this.$firebase.firestore.FieldValue.arrayUnion(this.fireUser.uid)
+        })
       }
     },
-    async go(arrow) {
-      if (!this.doc) throw Error("읽지 못했음");
-      const ref = this.$firebase
-        .firestore()
-        .collection("boards")
-        .doc(this.boardId)
-        .collection("articles")
-        .orderBy("createdAt", "desc");
-      let sn;
-      if (arrow < 0)
-        sn = await ref
-          .endBefore(this.doc)
-          .limitToLast(1)
-          .get();
-      else
-        sn = await ref
-          .startAfter(this.doc)
-          .limit(1)
-          .get();
-      if (sn.empty) throw Error("더이상 페이지가 없습니다");
-      const doc = sn.docs[0];
+    async go (arrow) {
+      if (!this.doc) return
+      const ref = this.$firebase.firestore()
+        .collection('boards').doc(this.boardId)
+        .collection('articles').orderBy('createdAt', 'desc')
+      let sn
+      if (arrow < 0) sn = await ref.endBefore(this.doc).limitToLast(1).get()
+      else sn = await ref.startAfter(this.doc).limit(1).get()
 
-      const us = this.$route.path.split("/");
-      us.pop();
-      us.push(doc.id);
-      this.$router.push({ path: us.join("/") });
+      if (sn.empty) return this.$toast.info('더이상 페이지가 없습니다')
+      const doc = sn.docs[0]
+
+      const us = this.$route.path.split('/')
+      us.pop()
+      us.push(doc.id)
+      this.$router.push({ path: us.join('/') })
     }
   }
-};
+}
 </script>
--- src/components/display-comment.vue
+++ src/components/display-comment.vue
@@ -3,13 +3,16 @@
     <v-card-title>
       <v-textarea
         v-model="comment"
-        rows="3"
         outlined
         label="댓글 작성"
-        append-icon="mdi-send"
+        placeholder="Ctrl + Enter로 작성 가능"
+        append-icon="mdi-comment-plus"
         @click:append="save"
+        @keypress.ctrl.enter="save"
         hide-details
-      ></v-textarea>
+        auto-grow
+        rows="1"
+        clearable />
     </v-card-title>
     <template v-for="(item, i) in items">
       <v-list-item :key="item.id">
@@ -17,119 +20,107 @@
           <display-user :user="item.user"></display-user>
         </v-list-item-action>
         <v-list-item-content>
-          <v-list-item-subtitle
-            class="black--text comment"
-            v-text="item.comment"
-          ></v-list-item-subtitle>
+          <v-list-item-subtitle class="black--text comment" v-text="item.comment"></v-list-item-subtitle>
           <v-list-item-subtitle class="font-italic">
             <display-time :time="item.createdAt"></display-time>
           </v-list-item-subtitle>
         </v-list-item-content>
         <v-list-item-action>
-          <v-btn icon @click="like(item)">
-            <v-icon :color="liked(item) ? 'success' : ''">mdi-thumb-up</v-icon>
-            <span>{{ item.likeCount }}</span>
+          <v-btn @click="like(item)" text>
+            <v-icon left :color="liked(item) ? 'success': ''">mdi-thumb-up</v-icon>
+            <span>{{item.likeCount}}</span>
           </v-btn>
         </v-list-item-action>
-        <v-list-item-action>
+        <v-list-item-action v-if="(fireUser && fireUser.uid === item.uid) || (user && user.level === 0)">
           <v-btn icon @click="remove(item)">
             <v-icon>mdi-delete</v-icon>
           </v-btn>
         </v-list-item-action>
       </v-list-item>
-      <v-divider :key="i"></v-divider>
+      <v-divider :key="i" v-if="i < items.length - 1"></v-divider>
     </template>
     <v-list-item v-if="lastDoc && items.length < article.commentCount">
-      <v-btn @click="more" v-intersect="onIntersect" text color="primary" block
-        >더보기</v-btn
-      >
+      <v-btn @click="more" v-intersect="onIntersect" text color="primary" block>더보기</v-btn>
     </v-list-item>
   </v-card>
 </template>
 <script>
-import { last } from "lodash";
-import DisplayTime from "@/components/display-time";
-import DisplayUser from "@/components/display-user";
-const LIMIT = 5;
+import { last } from 'lodash'
+import DisplayTime from '@/components/display-time'
+import DisplayUser from '@/components/display-user'
+const LIMIT = 5
 
 export default {
   components: { DisplayTime, DisplayUser },
-  props: ["article", "docRef"],
-  data() {
+  props: ['article', 'docRef'],
+  data () {
     return {
-      comment: "",
+      comment: '',
       items: [],
       unsubscribe: null,
       lastDoc: null
-    };
+    }
   },
   computed: {
-    user() {
-      return this.$store.state.user;
+    user () {
+      return this.$store.state.user
     },
-    fireUser() {
-      return this.$store.state.fireUser;
+    fireUser () {
+      return this.$store.state.fireUser
     }
   },
   watch: {
-    docRef() {
-      this.subscribe();
+    docRef () {
+      this.subscribe()
     }
   },
-  created() {
-    this.subscribe();
+  created () {
+    this.subscribe()
   },
-  destroyed() {
-    if (this.unsubscribe) this.unsubscribe();
+  destroyed () {
+    if (this.unsubscribe) this.unsubscribe()
   },
   methods: {
-    snapshotToItems(sn) {
-      this.lastDoc = last(sn.docs);
+    snapshotToItems (sn) {
+      this.lastDoc = last(sn.docs)
       sn.docs.forEach(doc => {
-        const exists = this.items.some(item => doc.id === item.id);
+        const exists = this.items.some(item => doc.id === item.id)
         if (!exists) {
-          const item = doc.data();
-          item.id = doc.id;
-          item.createdAt = item.createdAt.toDate();
-          item.updatedAt = item.updatedAt.toDate();
-          this.items.push(item);
+          const item = doc.data()
+          item.id = doc.id
+          item.createdAt = item.createdAt.toDate()
+          item.updatedAt = item.updatedAt.toDate()
+          this.items.push(item)
         }
-      });
+      })
       this.items.sort((before, after) => {
-        const beforeId = Number(before.id);
-        const afterId = Number(after.id);
-        return afterId - beforeId;
-      });
+        const beforeId = Number(before.id)
+        const afterId = Number(after.id)
+        return afterId - beforeId
+      })
     },
-    subscribe() {
-      if (this.unsubscribe) this.unsubscribe();
-      this.items = [];
-      this.unsubscribe = this.docRef
-        .collection("comments")
-        .orderBy("createdAt", "desc")
-        .limit(LIMIT)
-        .onSnapshot(sn => {
-          if (sn.empty) {
-            this.items = [];
-            return;
-          }
-          this.snapshotToItems(sn);
-        });
+    subscribe () {
+      if (this.unsubscribe) this.unsubscribe()
+      this.items = []
+      this.unsubscribe = this.docRef.collection('comments').orderBy('createdAt', 'desc').limit(LIMIT).onSnapshot(sn => {
+        if (sn.empty) {
+          this.items = []
+          return
+        }
+        this.snapshotToItems(sn)
+      })
     },
-    async more() {
-      if (!this.lastDoc) throw Error("더이상 데이터가 없습니다");
-      const sn = await this.docRef
-        .collection("comments")
-        .orderBy("createdAt", "desc")
-        .startAfter(this.lastDoc)
-        .limit(LIMIT)
-        .get();
-      this.snapshotToItems(sn);
+    async more () {
+      if (!this.lastDoc) throw Error('더이상 데이터가 없습니다')
+      const sn = await this.docRef.collection('comments').orderBy('createdAt', 'desc').startAfter(this.lastDoc).limit(LIMIT).get()
+      this.snapshotToItems(sn)
     },
-    onIntersect(entries, observer, isIntersecting) {
-      if (isIntersecting) this.more();
+    onIntersect (entries, observer, isIntersecting) {
+      if (isIntersecting) this.more()
     },
-    async save() {
+    async save () {
+      if (!this.fireUser) throw Error('로그인이 필요합니다')
+      if (this.comment.length > 10) throw Error('문자 허용치를 넘었습니다')
       const doc = {
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -142,63 +133,40 @@ export default {
         },
         likeCount: 0,
         likeUids: []
-      };
-      const id = doc.createdAt.getTime().toString();
-      // const batch = this.$firebase.firestore().batch()
-      // batch.update(this.docRef, { commentCount: this.$firebase.firestore.FieldValue.increment(1) })
-      // batch.set(this.docRef.collection('comments').doc(id), doc)
-      // await batch.commit()
-      this.docRef
-        .collection("comments")
-        .doc(id)
-        .set(doc);
-      this.comment = "";
-    },
-    liked(item) {
-      if (!this.fireUser) return false;
-      return item.likeUids.includes(this.fireUser.uid);
-    },
-    async like(comment) {
-      if (!this.fireUser) throw Error("로그인이 필요합니다");
-      if (this.liked(comment)) {
-        await this.docRef
-          .collection("comments")
-          .doc(comment.id)
-          .update({
-            likeCount: this.$firebase.firestore.FieldValue.increment(-1),
-            likeUids: this.$firebase.firestore.FieldValue.arrayRemove(
-              this.fireUser.uid
-            )
-          });
-      } else {
-        await this.docRef
-          .collection("comments")
-          .doc(comment.id)
-          .update({
-            likeCount: this.$firebase.firestore.FieldValue.increment(1),
-            likeUids: this.$firebase.firestore.FieldValue.arrayUnion(
-              this.fireUser.uid
-            )
-          });
       }
-      const doc = await this.docRef
-        .collection("comments")
-        .doc(comment.id)
-        .get();
-      const item = doc.data();
-      comment.likeCount = item.likeCount;
-      comment.likeUids = item.likeUids;
+      const id = doc.createdAt.getTime().toString()
+      this.docRef.collection('comments').doc(id).set(doc)
+      this.comment = ''
     },
-    async remove(comment) {
-      await this.docRef
-        .collection("comments")
-        .doc(comment.id)
-        .delete();
-      const i = this.items.findIndex(el => el.id === comment.id);
-      this.items.splice(i, 1);
+    liked (item) {
+      if (!this.fireUser) return false
+      return item.likeUids.includes(this.fireUser.uid)
+    },
+    async like (comment) {
+      if (!this.fireUser) throw Error('로그인이 필요합니다')
+      if (this.liked(comment)) {
+        await this.docRef.collection('comments').doc(comment.id).update({
+          likeCount: this.$firebase.firestore.FieldValue.increment(-1),
+          likeUids: this.$firebase.firestore.FieldValue.arrayRemove(this.fireUser.uid)
+        })
+      } else {
+        await this.docRef.collection('comments').doc(comment.id).update({
+          likeCount: this.$firebase.firestore.FieldValue.increment(1),
+          likeUids: this.$firebase.firestore.FieldValue.arrayUnion(this.fireUser.uid)
+        })
+      }
+      const doc = await this.docRef.collection('comments').doc(comment.id).get()
+      const item = doc.data()
+      comment.likeCount = item.likeCount
+      comment.likeUids = item.likeUids
+    },
+    async remove (comment) {
+      await this.docRef.collection('comments').doc(comment.id).delete()
+      const i = this.items.findIndex(el => el.id === comment.id)
+      this.items.splice(i, 1)
     }
   }
-};
+}
 </script>
 <style scoped>
 .comment {
