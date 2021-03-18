6946f6efdea811500ea1e69fde6de1d6435ba382
jukman <jukman@naver.com>
2021-03-18 13:54:42 +0900

    82 게시물에 유튜브 표시하기

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,37 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/home/')
+    path: "/",
+    name: "Home",
+    component: () => import("@/views/home/index")
   },
   {
-    path: '/board',
-    component: () => import('@/views/board')
+    path: "/board",
+    component: () => import("@/views/board")
   },
   {
-    path: '/board/:info',
-    component: () => import('@/views/board/info')
+    path: "/board/:info",
+    component: () => import("@/views/board/info")
   },
   {
-    path: '/board/:info/:article',
-    component: () => import('@/views/board/article')
+    path: "/board/:info/:article",
+    component: () => import("@/views/board/article")
   },
   {
-    path: '*',
-    name: 'error',
-    component: () => import('../views/error')
+    path: "*",
+    name: "error",
+    component: () => import("../views/error")
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
