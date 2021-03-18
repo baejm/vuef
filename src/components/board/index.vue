1fded526cd2034385aab5106a7ba6f6247b853fa
jukman <jukman@naver.com>
2021-03-18 09:20:07 +0900

    76 새로운 글 상태 표시하기

--- src/components/board/index.vue
+++ src/components/board/index.vue
@@ -3,30 +3,22 @@
     <v-card outlined :tile="$vuetify.breakpoint.xs" v-if="items.length">
       <v-toolbar color="transparent" dense flat>
         <v-toolbar-title>게시판 목록</v-toolbar-title>
-        <v-spacer />
+        <v-spacer/>
       </v-toolbar>
       <v-card-text>
         <v-row>
-          <v-col
-            cols="12"
-            sm="6"
-            md="4"
-            lg="3"
-            xl="2"
-            v-if="user && user.level === 0"
-          >
+          <v-col cols="12" sm="6" md="4" lg="3" xl="2" v-if="user && user.level === 0">
             <v-card height="100%">
               <v-subheader>
                 새로운 게시판 추가
               </v-subheader>
-              <v-divider />
+              <v-divider/>
               <v-card-text>
                 <v-text-field
                   v-model="boardId"
                   label="게시판 아이디"
                   placeholder="주소에 사용 될 문자입니다"
-                  outlined
-                />
+                  outlined />
               </v-card-text>
               <v-card-actions v-if="boardId">
                 <v-btn
@@ -34,58 +26,79 @@
                   x-large
                   color="primary"
                   text
-                  block
-                >
+                  block>
                   <v-icon left>mdi-plus</v-icon>
                   추가
                 </v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
-          <v-col
-            cols="12"
-            sm="6"
-            md="4"
-            lg="3"
-            xl="2"
-            v-for="item in items"
-            :key="item.id"
-          >
+          <v-col cols="12" sm="6" md="4" lg="3" xl="2" v-for="(item) in items" :key="item.id">
             <v-card height="100%">
               <v-subheader>
-                {{ item.title }}
-                <v-spacer />
+                <v-icon color="error" left v-if="newCheck(item.updatedAt)">mdi-fire</v-icon>
+                {{item.title}}
+                <v-spacer/>
                 <template v-if="user && user.level === 0">
-                  <v-btn icon :to="`${$route.path}/${item.id}?&action=write`">
+                  <v-btn
+                    icon
+                    :to="`${$route.path}/${item.id}?&action=write`">
                     <v-icon>mdi-pencil</v-icon>
                   </v-btn>
-                  <v-btn icon @click="remove(item)">
+                  <v-btn
+                    icon
+                    @click="remove(item)">
                     <v-icon>mdi-delete</v-icon>
                   </v-btn>
                 </template>
               </v-subheader>
-              <v-divider />
+              <v-divider/>
               <v-card-text>
-                <v-alert
-                  border="left"
-                  type="info"
-                  outlined
-                  class="white-space"
-                  >{{ item.description }}</v-alert
-                >
+                <v-alert border="left" type="info" outlined class="white-space">{{item.description}}</v-alert>
               </v-card-text>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    작성자
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    <display-user :user="item.user"></display-user>
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    작성일
+                  </v-list-item-title>
+                  <v-list-item-subtitle class="font-italic">
+                    <display-time :time="item.createdAt"></display-time>
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    수정일
+                  </v-list-item-title>
+                  <v-list-item-subtitle class="font-italic">
+                    <display-time :time="item.updatedAt"></display-time>
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
               <v-list-item>
                 <v-list-item-content>
                   <v-list-item-title>
                     게시물수
                   </v-list-item-title>
                   <v-list-item-subtitle class="font-italic">
-                    {{ item.count }}
+                    {{item.count}}
                   </v-list-item-subtitle>
                 </v-list-item-content>
               </v-list-item>
-              <v-divider />
-              <v-list-item :to="`${$route.path}/${item.id}`">
+              <v-divider/>
+              <v-list-item
+                :to="`${$route.path}/${item.id}`">
                 <v-list-item-content>
                   전체
                 </v-list-item-content>
@@ -95,14 +108,13 @@
                   </v-btn>
                 </v-list-item-action>
               </v-list-item>
-              <v-divider />
+              <v-divider/>
               <template v-for="(category, i) in item.categories">
                 <v-list-item
                   :key="category"
-                  :to="`${$route.path}/${item.id}?category=${category}`"
-                >
+                  :to="`${$route.path}/${item.id}?category=${category}`">
                   <v-list-item-content>
-                    {{ category }}
+                    {{category}}
                   </v-list-item-content>
                   <v-list-item-action>
                     <v-btn icon>
@@ -114,16 +126,15 @@
               </template>
             </v-card>
           </v-col>
-          <v-col cols="12" sm="6" md="4" lg="3" xl="2" v-if="lastDoc">
+          <v-col cols="12" sm="6" md="4" lg="3" xl="2"
+            v-if="lastDoc">
             <v-container fluid fill-height>
-              <v-btn
-                @click="more"
+              <v-btn @click="more"
                 v-intersect="onIntersect"
                 text
                 color="primary"
                 block
-                :loading="loading"
-              >
+                :loading="loading">
                 <v-icon>mdi-dots-horizontal</v-icon>더보기
               </v-btn>
             </v-container>
@@ -131,101 +142,103 @@
         </v-row>
       </v-card-text>
     </v-card>
-    <v-skeleton-loader v-else type="card"></v-skeleton-loader>
+    <v-container v-else fluid>
+      <v-row>
+        <v-col cols="12" sm="6" md="4" lg="3" xl="2" v-for="i in 4" :key="i">
+          <v-skeleton-loader type="card"></v-skeleton-loader>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-container>
 </template>
 <script>
-import { last } from "lodash";
-const LIMIT = 5;
+import { last } from 'lodash'
+import DisplayTime from '@/components/display-time'
+import DisplayUser from '@/components/display-user'
+import newCheck from '@/util/newCheck'
+const LIMIT = 5
 
 export default {
-  data() {
+  components: { DisplayTime, DisplayUser },
+  data () {
     return {
       unsubscribe: null,
       items: [],
       ref: null,
       lastDoc: null,
-      order: "createdAt",
-      sort: "desc",
-      boardId: "",
-      loading: false
-    };
+      order: 'createdAt',
+      sort: 'desc',
+      boardId: '',
+      loading: false,
+      newCheck
+    }
   },
   computed: {
-    user() {
-      return this.$store.state.user;
-    }
+    user () { return this.$store.state.user }
   },
-  created() {
-    this.subscribe();
+  created () {
+    this.subscribe()
   },
-  destroyed() {
-    if (this.unsubscribe) this.unsubscribe();
+  destroyed () {
+    if (this.unsubscribe) this.unsubscribe()
   },
   methods: {
-    snapshotToItems(sn) {
-      this.lastDoc = last(sn.docs);
+    snapshotToItems (sn) {
+      this.lastDoc = last(sn.docs)
       sn.docs.forEach(doc => {
-        const findItem = this.items.find(item => doc.id === item.id);
-        const item = doc.data();
+        const findItem = this.items.find(item => doc.id === item.id)
+        const item = doc.data()
         if (!findItem) {
-          item.id = doc.id;
-          item.createdAt = item.createdAt.toDate();
-          item.updatedAt = item.updatedAt.toDate();
-          this.items.push(item);
+          item.id = doc.id
+          item.createdAt = item.createdAt.toDate()
+          item.updatedAt = item.updatedAt.toDate()
+          this.items.push(item)
         } else {
-          findItem.category = item.category;
-          findItem.title = item.title;
-          findItem.count = item.count;
-          findItem.description = item.description;
-          findItem.categories = item.categories;
-          findItem.tags = item.tags;
-          findItem.updatedAt = item.updatedAt.toDate();
+          findItem.category = item.category
+          findItem.title = item.title
+          findItem.count = item.count
+          findItem.description = item.description
+          findItem.categories = item.categories
+          findItem.tags = item.tags
+          findItem.updatedAt = item.updatedAt.toDate()
         }
-      });
+      })
       this.items.sort((before, after) => {
-        return (
-          Number(after.createdAt.getTime()) - Number(before.createdAt.getTime())
-        );
-      });
+        return Number(after.createdAt.getTime()) - Number(before.createdAt.getTime())
+      })
     },
-    subscribe() {
-      this.ref = this.$firebase
-        .firestore()
-        .collection("boards")
-        .orderBy(this.order, this.sort)
-        .limit(LIMIT);
+    subscribe () {
+      this.ref = this.$firebase.firestore()
+        .collection('boards')
+        .orderBy(this.order, this.sort).limit(LIMIT)
       this.unsubscribe = this.ref.onSnapshot(sn => {
         if (sn.empty) {
-          this.items = [];
-          return;
+          this.items = []
+          return
         }
-        this.snapshotToItems(sn);
-      });
+        this.snapshotToItems(sn)
+      })
     },
-    async more() {
-      if (!this.lastDoc) throw Error("더이상 데이터가 없습니다");
-      if (this.loading) return;
-      this.loading = true;
+    async more () {
+      if (!this.lastDoc) throw Error('더이상 데이터가 없습니다')
+      if (this.loading) return
+      this.loading = true
       try {
-        const sn = await this.ref.startAfter(this.lastDoc).get();
-        this.snapshotToItems(sn);
+        const sn = await this.ref.startAfter(this.lastDoc).get()
+        this.snapshotToItems(sn)
       } finally {
-        this.loading = false;
+        this.loading = false
       }
     },
-    onIntersect(entries, observer, isIntersecting) {
-      if (isIntersecting) this.more();
+    onIntersect (entries, observer, isIntersecting) {
+      if (isIntersecting) this.more()
     },
-    async remove(item) {
-      await this.$firebase
-        .firestore()
-        .collection("boards")
-        .doc(item.id)
-        .delete();
-      const i = this.items.findIndex(el => el.id === item.id);
-      this.items.splice(i, 1);
+    async remove (item) {
+      await this.$firebase.firestore()
+        .collection('boards').doc(item.id).delete()
+      const i = this.items.findIndex(el => el.id === item.id)
+      this.items.splice(i, 1)
     }
   }
-};
+}
 </script>
