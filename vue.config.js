1451cc074a781535cc600fa0bdabb83686bf98bb
jukman <jukman@naver.com>
2021-03-19 16:09:43 +0900

    105 메인페이지 메타정보 동적 생성하기

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,13 @@ module.exports = {
   outputDir: "docs",
   devServer: {
     overlay: false
+  },
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = process.env.VUE_APP_SITE_TITLE;
+      args[0].description = process.env.VUE_APP_SITE_DESCRIPTION;
+      args[0].image = process.env.VUE_APP_SITE_IMAGE;
+      return args;
+    });
   }
 };
