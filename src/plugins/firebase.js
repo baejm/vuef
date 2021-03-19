a681fbfc69e42c50bcfdef2ee3914b47b38dadf6
jukman <jukman@naver.com>
2021-03-19 15:00:50 +0900

    102 검색어 인덱싱 하기

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,30 +1,33 @@
-import Vue from 'vue'
-import * as firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firebase-database'
-import 'firebase/firebase-firestore'
-import 'firebase/firebase-storage'
-import firebaseConfig from '../../firebaseConfig'
-import store from '../store/'
+import Vue from "vue";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firebase-database";
+import "firebase/firebase-firestore";
+import "firebase/firebase-storage";
+import firebaseConfig from "../../firebaseConfig";
+import store from "@/store";
 
-firebase.initializeApp(firebaseConfig)
-let unsubscribe = null
+firebase.initializeApp(firebaseConfig);
 
-const subscribe = (fu) => {
-  const ref = firebase.firestore().collection('users').doc(fu.uid)
+let unsubscribe = null;
+
+const subscribe = fu => {
+  const ref = firebase
+    .firestore()
+    .collection("users")
+    .doc(fu.uid);
   unsubscribe = ref.onSnapshot(doc => {
-    if (doc.exists) store.commit('setUser', doc.data())
-  }, console.error)
-}
+    if (doc.exists) store.commit("setUser", doc.data());
+  }, console.error);
+};
 
-firebase.auth().onAuthStateChanged((fu) => {
-  store.commit('setFireUser', fu)
+firebase.auth().onAuthStateChanged(fu => {
+  store.commit("setFireUser", fu);
   if (!fu) {
-    store.commit('setUser', null)
-    if (unsubscribe) unsubscribe()
-    return
+    store.commit("setUser", null);
+    if (unsubscribe) unsubscribe();
+    return;
   }
-  subscribe(fu)
-})
-
-Vue.prototype.$firebase = firebase
+  subscribe(fu);
+});
+Vue.prototype.$firebase = firebase;
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuetify, { VSnackbar, VBtn, VIcon } from 'vuetify/lib'
+import VuetifyToast from 'vuetify-toast-snackbar'
 
 import en from 'vuetify/es5/locale/en'
 import ko from 'vuetify/es5/locale/ko'
@@ -17,11 +18,7 @@ const theme = {
       primary: '#344059',
       secondary: '#BFBFBF', // #FFCDD2
       accent: '#2C4027',
-      info: '#4E7DA6',
-      success: '#26A699',
-      warning: '#F29727',
-      error: '#F24C3D'
-
+      info: '#4E7DA6'
       // success: '#2C4027'
     }
     // dark: {
@@ -37,6 +34,33 @@ const VuetifyObj = new Vuetify({
     current: 'ko'
   },
   theme
+})
+Vue.use(VuetifyToast, {
+  x: 'right', // default
+  y: 'bottom', // default
+  color: 'info', // default
+  icon: 'mdi-information',
+  iconColor: '', // default
+  classes: [
+    'body-2'
+  ],
+  timeout: 3000, // default
+  dismissable: true, // default
+  multiLine: false, // default
+  vertical: false, // default
+  queueable: false, // default
+  showClose: false, // default
+  closeText: '', // default
+  closeIcon: 'close', // default
+  closeColor: '', // default
+  slot: [], // default
+  shorts: {
+    custom: {
+      color: 'purple'
+    }
+  },
+  property: '$toast', // default
+  $vuetify: VuetifyObj.framework
 })
 
 export default VuetifyObj
