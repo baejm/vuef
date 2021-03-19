92dd55369f044e2dd534e1c2bcf7c1fd4788c83b
jukman <jukman@naver.com>
2021-03-19 14:25:27 +0900

    100 검색 내용 표시하기

--- src/components/board/article/index.vue
+++ src/components/board/article/index.vue
@@ -1,18 +1,38 @@
 <template>
   <v-container fluid v-if="!loaded">
-    <v-skeleton-loader type="article" v-for="i in 3" :key="i"></v-skeleton-loader>
+    <v-skeleton-loader
+      type="article"
+      v-for="i in 3"
+      :key="i"
+    ></v-skeleton-loader>
   </v-container>
   <v-container fluid v-else-if="loaded && !items.length">
     <v-alert type="warning" border="left" class="mb-0">
-      게시물이 없습니다 <v-icon>mdi-plus</v-icon> 버튼을 눌러서 게시물을 작성하세요~
+      게시물이 없습니다 <v-icon>mdi-plus</v-icon> 버튼을 눌러서 게시물을
+      작성하세요~
     </v-alert>
   </v-container>
   <v-container fluid v-else class="pa-0">
     <template v-if="board.type === '일반'">
-      <list-compact v-if="$store.state.boardTypeList" :items="items" :boardId="boardId" :category="category"/>
-      <list-normal v-else :items="items" :boardId="boardId" :category="category"/>
+      <list-compact
+        v-if="$store.state.boardTypeList"
+        :items="items"
+        :boardId="boardId"
+        :category="category"
+      />
+      <list-normal
+        v-else
+        :items="items"
+        :boardId="boardId"
+        :category="category"
+      />
     </template>
-    <list-gallery v-else :items="items" :boardId="boardId" :category="category"/>
+    <list-gallery
+      v-else
+      :items="items"
+      :boardId="boardId"
+      :category="category"
+    />
     <v-list-item v-if="lastDoc && items.length < board.count">
       <v-btn
         @click="more"
@@ -20,154 +40,168 @@
         text
         color="primary"
         block
-        :loading="loading">
+        :loading="loading"
+      >
         <v-icon>mdi-dots-horizontal</v-icon>더보기
       </v-btn>
     </v-list-item>
   </v-container>
 </template>
 <script>
-import { last } from 'lodash'
-import ListCompact from './components/list-compact'
-import ListNormal from './components/list-normal'
-import ListGallery from './components/list-gallery'
+import { last } from "lodash";
+import ListCompact from "./components/list-compact";
+import ListNormal from "./components/list-normal";
+import ListGallery from "./components/list-gallery";
 
-const LIMIT = 5
+const LIMIT = 5;
 
 export default {
   components: { ListCompact, ListNormal, ListGallery },
-  props: ['board', 'boardId', 'category', 'tag'],
-  data () {
+  props: ["board", "boardId", "category", "tag"],
+  data() {
     return {
       items: [],
       unsubscribe: null,
       ref: null,
       lastDoc: null,
-      order: 'createdAt',
-      sort: 'desc',
+      order: "createdAt",
+      sort: "desc",
       loading: false,
       loaded: false
-    }
+    };
   },
   computed: {
-    fireUser () {
-      return this.$store.state.fireUser
+    fireUser() {
+      return this.$store.state.fireUser;
     },
-    getCategory () {
-      if (!this.category) return '전체'
-      return this.category
+    getCategory() {
+      if (!this.category) return "전체";
+      return this.category;
     }
   },
   watch: {
-    boardId () {
-      this.subscribe()
+    boardId() {
+      this.subscribe();
     },
-    category () {
-      this.subscribe()
+    category() {
+      this.subscribe();
     }
   },
-  created () {
-    this.subscribe()
+  created() {
+    this.subscribe();
   },
-  destroyed () {
-    if (this.unsubscribe) this.unsubscribe()
+  destroyed() {
+    if (this.unsubscribe) this.unsubscribe();
   },
   methods: {
-    snapshotToItems (sn) {
-      this.lastDoc = last(sn.docs)
+    snapshotToItems(sn) {
+      this.lastDoc = last(sn.docs);
       sn.docs.forEach(doc => {
-        const findItem = this.items.find(item => doc.id === item.id)
-        const item = doc.data()
+        const findItem = this.items.find(item => doc.id === item.id);
+        const item = doc.data();
         if (!findItem) {
-          item.id = doc.id
-          item.createdAt = item.createdAt.toDate()
-          item.updatedAt = item.updatedAt.toDate()
-          item.overlay = false
-          this.items.push(item)
+          item.id = doc.id;
+          item.createdAt = item.createdAt.toDate();
+          item.updatedAt = item.updatedAt.toDate();
+          item.overlay = false;
+          this.items.push(item);
         } else {
           if (findItem.summary !== item.summary) {
-            findItem.summary = ''
+            findItem.summary = "";
             setTimeout(() => {
-              findItem.summary = item.summary
-            }, 1000)
+              findItem.summary = item.summary;
+            }, 1000);
           }
-          findItem.title = item.title
-          findItem.readCount = item.readCount
-          findItem.commentCount = item.commentCount
-          findItem.likeCount = item.likeCount
-          findItem.likeUids = item.likeUids
-          findItem.categories = item.categories
-          findItem.tags = item.tags
-          findItem.updatedAt = item.updatedAt.toDate()
-          findItem.important = item.important
+          findItem.title = item.title;
+          findItem.readCount = item.readCount;
+          findItem.commentCount = item.commentCount;
+          findItem.likeCount = item.likeCount;
+          findItem.likeUids = item.likeUids;
+          findItem.categories = item.categories;
+          findItem.tags = item.tags;
+          findItem.updatedAt = item.updatedAt.toDate();
+          findItem.important = item.important;
         }
-      })
+      });
       this.items.sort((before, after) => {
-        if (after.important > before.important) return 1
-        else if (after.important < before.important) return -1
-        return Number(after.id) - Number(before.id)
-      })
+        if (after.important > before.important) return 1;
+        else if (after.important < before.important) return -1;
+        return Number(after.id) - Number(before.id);
+      });
     },
-    subscribe (arrow) {
-      if (this.unsubscribe) this.unsubscribe()
-      this.items = []
+    subscribe(arrow) {
+      if (this.unsubscribe) this.unsubscribe();
+      this.items = [];
       if (!this.category) {
-        this.ref = this.$firebase.firestore()
-          .collection('boards').doc(this.boardId)
-          .collection('articles')
-          .orderBy('important', 'desc')
+        this.ref = this.$firebase
+          .firestore()
+          .collection("boards")
+          .doc(this.boardId)
+          .collection("articles")
+          .orderBy("important", "desc")
           .orderBy(this.order, this.sort)
-          .limit(LIMIT)
+          .limit(LIMIT);
       } else {
-        this.ref = this.$firebase.firestore()
-          .collection('boards').doc(this.boardId)
-          .collection('articles')
-          .where('category', '==', this.category)
-          .orderBy('important', 'desc')
+        this.ref = this.$firebase
+          .firestore()
+          .collection("boards")
+          .doc(this.boardId)
+          .collection("articles")
+          .where("category", "==", this.category)
+          .orderBy("important", "desc")
           .orderBy(this.order, this.sort)
-          .limit(LIMIT)
+          .limit(LIMIT);
       }
-      this.loaded = false
+      console.log(arrow);
+      this.loaded = false;
       this.unsubscribe = this.ref.onSnapshot(sn => {
-        this.loaded = true
+        this.loaded = true;
         if (sn.empty) {
-          this.items = []
-          return
+          this.items = [];
+          return;
         }
-        this.snapshotToItems(sn)
-        this.setMeta(this.board)
-      })
+        this.snapshotToItems(sn);
+        this.setMeta(this.board);
+      });
     },
-    setMeta (item) {
-      const descriptionNode = document.querySelector('head meta[name="description"]')
-      const ogTitleNode = document.querySelector('head meta[property="og:title"]')
-      const ogDescriptionNode = document.querySelector('head meta[property="og:description"]')
-      const ogImageNode = document.querySelector('head meta[property="og:image"]')
+    setMeta(item) {
+      const descriptionNode = document.querySelector(
+        'head meta[name="description"]'
+      );
+      const ogTitleNode = document.querySelector(
+        'head meta[property="og:title"]'
+      );
+      const ogDescriptionNode = document.querySelector(
+        'head meta[property="og:description"]'
+      );
+      const ogImageNode = document.querySelector(
+        'head meta[property="og:image"]'
+      );
 
-      const title = item.title + ' ' + this.getCategory + ' 목록 : memi'
-      const description = item.description.substr(0, 80)
-      const image = '/logo.png'
+      const title = item.title + " " + this.getCategory + " 목록 : memi";
+      const description = item.description.substr(0, 80);
+      const image = "/logo.png";
 
-      document.title = title
-      descriptionNode.setAttribute('content', description)
-      ogTitleNode.setAttribute('content', title)
-      ogDescriptionNode.setAttribute('content', description)
-      ogImageNode.setAttribute('content', image)
+      document.title = title;
+      descriptionNode.setAttribute("content", description);
+      ogTitleNode.setAttribute("content", title);
+      ogDescriptionNode.setAttribute("content", description);
+      ogImageNode.setAttribute("content", image);
     },
-    async more () {
-      if (!this.lastDoc) throw Error('더이상 데이터가 없습니다')
-      if (this.loading) return
-      this.loading = true
+    async more() {
+      if (!this.lastDoc) throw Error("더이상 데이터가 없습니다");
+      if (this.loading) return;
+      this.loading = true;
       try {
-        const sn = await this.ref.startAfter(this.lastDoc).get()
-        this.snapshotToItems(sn)
+        const sn = await this.ref.startAfter(this.lastDoc).get();
+        this.snapshotToItems(sn);
       } finally {
-        this.loading = false
+        this.loading = false;
       }
     },
-    onIntersect (entries, observer, isIntersecting) {
-      if (isIntersecting) this.more()
+    onIntersect(entries, observer, isIntersecting) {
+      if (isIntersecting) this.more();
     }
   }
-}
+};
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,41 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/home/')
+    path: "/",
+    name: "Home",
+    component: () => import("@/views/home/index")
   },
   {
-    path: '/board',
-    component: () => import('@/views/board')
+    path: "/board",
+    component: () => import("@/views/board")
   },
   {
-    path: '/board/:info',
-    component: () => import('@/views/board/info')
+    path: "/board/:info",
+    component: () => import("@/views/board/info")
   },
   {
-    path: '/board/:info/:article',
-    component: () => import('@/views/board/article')
+    path: "/board/:info/:article",
+    component: () => import("@/views/board/article")
   },
   {
-    path: '/search',
-    component: () => import('@/views/board/search')
+    path: "/search",
+    component: () => import("@/views/board/search")
   },
   {
-    path: '*',
-    name: 'error',
-    component: () => import('../views/error')
+    path: "*",
+    name: "error",
+    component: () => import("../views/error")
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
