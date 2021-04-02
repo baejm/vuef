1942323889a2ed12b9937d932bf470e6705d48aa
baejm <jukman81@gmail.com>
2021-04-02 10:25:14 +0900

    정보수정

--- src/util/setMeta.js
+++ src/util/setMeta.js
@@ -1,16 +1,20 @@
-export default (item) => {
-  const title = item.title + ' : memi'
-  const description = item.description
-  const image = item.image
+export default item => {
+  const title = item.title + " : baejm";
+  const description = item.description;
+  const image = item.image;
 
-  const descriptionNode = document.querySelector('head meta[name="description"]')
-  const ogTitleNode = document.querySelector('head meta[property="og:title"]')
-  const ogDescriptionNode = document.querySelector('head meta[property="og:description"]')
-  const ogImageNode = document.querySelector('head meta[property="og:image"]')
+  const descriptionNode = document.querySelector(
+    'head meta[name="description"]'
+  );
+  const ogTitleNode = document.querySelector('head meta[property="og:title"]');
+  const ogDescriptionNode = document.querySelector(
+    'head meta[property="og:description"]'
+  );
+  const ogImageNode = document.querySelector('head meta[property="og:image"]');
 
-  document.title = title
-  descriptionNode.setAttribute('content', description)
-  ogTitleNode.setAttribute('content', title)
-  ogDescriptionNode.setAttribute('content', description)
-  ogImageNode.setAttribute('content', image)
-}
+  document.title = title;
+  descriptionNode.setAttribute("content", description);
+  ogTitleNode.setAttribute("content", title);
+  ogDescriptionNode.setAttribute("content", description);
+  ogImageNode.setAttribute("content", image);
+};
